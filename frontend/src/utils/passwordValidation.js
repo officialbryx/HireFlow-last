9aea0d93fbf7a475a70bb4b67bc5f624e2deca59
bryx <bryxph@gmail.com>
2025-03-31 13:25:28 +0800

    Update passwordValidation.js

--- frontend/src/utils/passwordValidation.js
+++ frontend/src/utils/passwordValidation.js
@@ -14,7 +14,9 @@ export const validatePassword = (password) => {
   const hasUpperCase = /[A-Z]/.test(password);
   const hasLowerCase = /[a-z]/.test(password);
   const hasNumbers = /\d/.test(password);
-  const hasSpecialChar = /[@#$%&*!]/.test(password);
+  const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~]/.test(
+    password
+  );
 
   const requirements = [
     {
@@ -39,7 +41,7 @@ export const validatePassword = (password) => {
     },
     {
       id: "special",
-      message: "Contains special character (@#$%&*!)",
+      message: "Contains special character",
       isMet: hasSpecialChar,
     },
   ];
